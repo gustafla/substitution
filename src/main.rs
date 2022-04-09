88d0a1aa6ad9365b5a5ff90cc1392842a2b974a3
Lauri Gustafsson <me@gustafla.space>
2022-04-09 15:44:15 +0300

    Implement CLI --in-place functionality

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@
 // "Include" src/io.rs in the main CLI here
 mod io;
 
-use clap::Parser;
+use clap::{ArgGroup, Parser};
 use color_eyre::{
     eyre::{eyre, Context},
     Result,
@@ -27,7 +27,8 @@ use std::{
 /// Main command line argument structure
 #[derive(Parser)]
 #[clap(author, version, about)]
-#[clap(propagate_version = true)]
+// Deny using -i and -o at the same time
+#[clap(group(ArgGroup::new("output").args(&["in-place", "output-file"])))]
 struct Cli {
     /// Overwrite the contents of the input file
     #[clap(long, short)]
@@ -66,35 +67,51 @@ impl std::str::FromStr for Mode {
     }
 }
 
+/// Read everything from stdin/file specified in CLI options
+fn read_input(opts: &Cli) -> Result<String> {
+    let mut text = String::with_capacity(4096);
+    let mut input: io::Input = opts.path.clone().try_into()?;
+    BufReader::new(input.as_mut())
+        .read_to_string(&mut text)
+        .wrap_err(format!("Cannot read from {}", input))?;
+    Ok(text)
+}
+
+/// Process all text and write to output
+fn run(mode: &Mode, text: &str, output: &mut io::Output) -> Result<()> {
+    // Run and write the result out
+    {
+        let mut writer = BufWriter::new(output.as_mut());
+        writeln!(
+            writer,
+            "{}",
+            match mode {
+                Mode::Decrypt => substitution::decrypt(text),
+                Mode::Encrypt => substitution::encrypt(text),
+            }
+        )
+    }
+    .wrap_err(format!("Cannot write to {}", output))
+}
+
 fn main() -> Result<()> {
     // Install color_eyre's panic- and error report handlers
     color_eyre::install()?;
 
     // Parse CLI arguments and read the input
     let opts = Cli::parse();
-    let mut text = String::with_capacity(4096);
-    let mut input: io::Input = opts.path.clone().try_into()?;
-    BufReader::new(input.as_mut())
-        .read_to_string(&mut text)
-        .wrap_err(format!("Cannot read from {}", input))?;
 
-    // Open output
-    let mut output: io::Output = opts.path.try_into()?;
-    {
-        // Buffer output to improve performance
-        let mut output = BufWriter::new(output.as_mut());
+    // Read input
+    let text = read_input(&opts)?;
 
-        // Run and write the result out
-        writeln!(
-            output,
-            "{}",
-            match opts.mode {
-                Mode::Decrypt => substitution::decrypt(&text),
-                Mode::Encrypt => substitution::encrypt(&text),
-            }
-        )
+    // Determine output from CLI
+    let mut output: io::Output = if opts.in_place {
+        opts.path
+    } else {
+        opts.output_file
     }
-    .wrap_err(format!("Cannot write to {}", output))?;
+    .try_into()?;
 
-    Ok(())
+    // Process the input and write to output
+    run(&opts.mode, &text, &mut output)
 }
