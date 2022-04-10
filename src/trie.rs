00455d8ae25b5b150fb5465205c4d9d882796467
Lauri Gustafsson <me@gustafla.space>
2022-04-10 14:56:47 +0300

    trie: Add more tests and error types

--- src/trie.rs
+++ src/trie.rs
@@ -1,13 +1,25 @@
+use thiserror::Error;
+
 /// Type for the size of the trie's alphabet
 pub type AlphabetSize = usize;
 
-/// Error type which will be returned when a key cannot be used with given alphabet size
-/// E.g. the key has value 19 but alphabet size is 10
-#[derive(Debug)]
-pub struct KeyNotInAlphabet;
+/// Errors that trie and key operations may return
+#[derive(Error, Debug)]
+pub enum Error {
+    /// Error which will be returned when a key cannot be used with given alphabet size
+    /// E.g. the key has value 19 but alphabet size is 10
+    #[error("value {value} in key does not fit in alphabet size {size}")]
+    KeyNotInAlphabet { value: usize, size: usize },
+    /// char does not fit in an usize, but Key's impl FromStr is used
+    #[error("char {value} does not fit in an usize, thus cannot be used in trie")]
+    CharCannotIndex {
+        value: char,
+        source: std::num::TryFromIntError,
+    },
+}
 
 /// Type used for looking up node indices from key's elements
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug)]
 pub struct KeyIndex<const R: AlphabetSize> {
     inner: usize,
 }
@@ -23,17 +35,17 @@ impl<const R: AlphabetSize> std::ops::Deref for KeyIndex<R> {
 
 // The only way to create KeyIndex. Checks the invariant that the index is lower than alphabet size
 impl<const R: AlphabetSize> TryFrom<usize> for KeyIndex<R> {
-    type Error = KeyNotInAlphabet;
+    type Error = Error;
 
     fn try_from(value: usize) -> Result<Self, Self::Error> {
         (value < R)
             .then(|| Self { inner: value })
-            .ok_or(KeyNotInAlphabet)
+            .ok_or(Error::KeyNotInAlphabet { value, size: R })
     }
 }
 
 /// Key for trie operations
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct Key<const R: AlphabetSize> {
     buf: Vec<KeyIndex<R>>,
 }
@@ -47,10 +59,28 @@ impl<const R: AlphabetSize> Key<R> {
 
 impl<const R: AlphabetSize> Key<R> {
     // Convert a u8 slice (byte string) to Key
-    pub fn from_bytes(slice: &[u8]) -> Result<Self, KeyNotInAlphabet> {
+    // This cannot be (for now) TryFrom<AsRef<[u8]>> because of conflicting blanket impl in core
+    pub fn from_bytes(slice: &[u8]) -> Result<Self, Error> {
         let mut buf = Vec::with_capacity(slice.len());
         for value in slice {
             buf.push(usize::from(*value).try_into()?);
+        }
+        Ok(Self { buf })
+    }
+}
+
+impl<const R: AlphabetSize> std::str::FromStr for Key<R> {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut buf = Vec::with_capacity(s.len());
+        for c in s.chars() {
+            let u: u32 = c.into();
+            let us: usize = u.try_into().map_err(|e| Error::CharCannotIndex {
+                value: c,
+                source: e,
+            })?;
+            buf.push(us.try_into()?);
         }
         Ok(Self { buf })
     }
@@ -186,5 +216,51 @@ mod test {
         let mut set = Set::<R>::new();
         set.insert(&key);
         assert!(set.contains(&key))
+    }
+
+    #[test]
+    fn insertion_subkey_not_contained() {
+        const R: AlphabetSize = 128;
+        let key = Key::<R>::from_bytes(b"hello").unwrap();
+        let mut set = Set::<R>::new();
+        set.insert(&key);
+        let key = Key::<R>::from_bytes(b"hell").unwrap();
+        assert!(!set.contains(&key))
+    }
+
+    #[test]
+    fn multiple_insertions() {
+        const R: AlphabetSize = 128;
+        let keys: Vec<Key<R>> = ["apples", "oranges", "bananas"]
+            .iter()
+            .map(|str| str.parse().unwrap())
+            .collect();
+        let false_keys: Vec<Key<R>> = ["apple", "orangee", "bananasplit", ""]
+            .iter()
+            .map(|str| str.parse().unwrap())
+            .collect();
+
+        let mut set = Set::<R>::new();
+        for key in &keys {
+            set.insert(key);
+        }
+        for key in &false_keys {
+            assert!(!set.contains(key))
+        }
+        for key in &keys {
+            assert!(set.contains(key))
+        }
+    }
+
+    #[test]
+    fn key_from_str_error() {
+        use std::str::FromStr;
+        assert!(matches!(
+            Key::<96>::from_str("Hello!"),
+            Err(Error::KeyNotInAlphabet {
+                value: 101,
+                size: 96,
+            })
+        ))
     }
 }
